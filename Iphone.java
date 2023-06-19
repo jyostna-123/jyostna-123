dc066408ca9fdf45bdc36010946b7ccbb1f18df7
jyostna-123 <137049003+jyostna-123@users.noreply.github.com>
2023-06-19 16:55:15 +0800

    Add files via upload

---
+++ Iphone.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class Iphone {
+public void makeCall() {
+System.out.println("make a new local call");	
+}
+public void model() {
+	System.out.println("this is iphone 14 model");
+}
+}
---
+++ SmartPhone.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class SmartPhone {
+
+	public static void main(String[] args) {
+		Iphone test=new Iphone();
+		test.makeCall();
+		test.model();
+
+}
+}
